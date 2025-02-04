0ec375bcb847e9882469bd1ca6211f76120a895e
zerozawa <zekaixie37@gmail.com>
2025-02-04 17:29:56 +0800

    fix: network

--- system/network.nix
+++ system/network.nix
@@ -10,24 +10,6 @@ in
   networking = {
     inherit hostName;
     firewall.enable = false;
-    nameservers = [ router ];
-    timeServers = [ router ];
-    defaultGateway = {
-      address = router;
-      interface = interface;
-    };
-    interfaces = {
-      ${interface} = {
-        mtu = 9000;
-        ipv4 = {
-          addresses = [
-            {
-              address = ip;
-              inherit prefixLength;
-            }
-          ];
-        };
-      };
-    };
+    networkmanager.enable = true;
   };
 }
