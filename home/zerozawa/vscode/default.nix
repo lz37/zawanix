2d1b51098eca681dfa4b2da677e22a9a50eb32d3
zerozawa <zekaixie37@gmail.com>
2025-03-27 00:20:56 +0800

    feat: simplify2

--- home/zerozawa/vscode/default.nix
+++ home/zerozawa/vscode/default.nix
@@ -1,11 +1,46 @@
-{ pkgs, ... }:
+{ pkgs, ... }@inputs:
+let
+  merge-vscode-modules = import ./common/utils.nix;
+  merge-imports = merge-vscode-modules ({ inherit pkgs; } // inputs);
+in
 {
-  imports = [
-    ./default
-  ];
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = false;
     package = pkgs.vscode;
+    profiles = {
+      default =
+        (merge-imports [
+          (import ./common/topics/base.nix)
+          (import ./common/topics/gui.nix)
+          (import ./common/topics/copilot.nix)
+          (import ./common/topics/docker.nix)
+          (import ./common/topics/gitlens.nix)
+          (import ./common/topics/lang.nix)
+          (import ./common/topics/markdown.nix)
+          (import ./common/topics/nix.nix)
+          (import ./common/topics/remote/ssh.nix)
+          (import ./common/topics/remote/liveshare.nix)
+          (import ./common/topics/frontend/base.nix)
+          (import ./common/topics/frontend/prettier.nix)
+          (import ./common/topics/settingfile/yaml.nix)
+          (import ./common/topics/settingfile/xml.nix)
+        ])
+        // {
+          enableUpdateCheck = false;
+          enableExtensionUpdateCheck = false;
+        };
+      devcontainer = (
+        merge-imports [
+          (import ./common/topics/base.nix)
+          (import ./common/topics/gui.nix)
+          (import ./common/topics/lang.nix)
+          (import ./common/topics/docker.nix)
+          (import ./common/topics/copilot.nix)
+          (import ./common/topics/gitlens.nix)
+          (import ./common/topics/remote/devcontainer.nix)
+        ]
+      );
+    };
   };
 }
