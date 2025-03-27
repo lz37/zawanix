6f69f26210c8eca8f77239657e8472e1ce9487a0
zerozawa <zekaixie37@gmail.com>
2025-03-27 10:17:07 +0800

    feat: vscode all attrs

--- home/zerozawa/vscode/common/utils.nix
+++ home/zerozawa/vscode/common/utils.nix
@@ -7,6 +7,9 @@ let
       keybindings = [ ];
       settings = { };
       extensions = [ ];
+      globalSnippets = { };
+      languageSnippets = { };
+      tasks = [ ];
     }
     // item
   ) returns;
@@ -15,4 +18,14 @@ in
   keybindings = builtins.concatMap (item: item.keybindings) returns-fallback;
   extensions = builtins.concatMap (item: item.extensions) returns-fallback;
   userSettings = input.lib.mergeAttrsList (builtins.map (item: item.settings) returns-fallback);
+  globalSnippets = input.lib.mergeAttrsList (
+    builtins.map (item: item.globalSnippets) returns-fallback
+  );
+  languageSnippets = input.lib.mergeAttrsList (
+    builtins.map (item: item.languageSnippets) returns-fallback
+  );
+  userTasks = {
+    version = "2.0.0";
+    tasks = builtins.concatMap (item: item.tasks) returns-fallback;
+  };
 }
