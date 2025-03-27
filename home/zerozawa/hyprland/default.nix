a7f8312f0fcc2ce098367d163d3726ece69f9130
zerozawa <zekaixie37@gmail.com>
2025-03-27 17:04:38 +0800

    feat: enable hyprland

--- home/zerozawa/hyprland/default.nix
+++ home/zerozawa/hyprland/default.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
 }
