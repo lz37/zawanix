f25c99364ffa73753201b52d08242372b50e4e5b
zerozawa <zekaixie37@gmail.com>
2025-03-28 01:35:59 +0800

    feat: hypr

--- hardware/nvidia/gpu.nix
+++ hardware/nvidia/gpu.nix
@@ -9,6 +9,7 @@
   hardware.graphics = {
     enable = true;
   };
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
--- home/zerozawa/hyprland/default.nix
+++ home/zerozawa/hyprland/default.nix
@@ -1,10 +1,37 @@
-{ inputs, pkgs, ... }@others:
+{
+  inputs,
+  pkgs,
+  isNvidiaGPU,
+  lib,
+  ...
+}@others:
 
 let
-  system-hyprland-settings = import ../../../system/hyprland.nix ({ inherit inputs pkgs; } // others);
+  system-hyprland-settings = import ../../../system/hyprland.nix (
+    {
+      inherit
+        inputs
+        pkgs
+        isNvidiaGPU
+        lib
+        ;
+    }
+    // others
+  );
 in
 {
   wayland.windowManager.hyprland = {
     inherit (system-hyprland-settings.programs.hyprland) enable package portalPackage;
+    xwayland.enable = true;
+    settings = {
+      env =
+        (lib.optionals isNvidiaGPU [
+          "LIBVA_DRIVER_NAME,nvidia"
+          "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        ])
+        ++ [
+          "NIXOS_OZONE_WL,1"
+        ];
+    };
   };
 }
