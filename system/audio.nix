d9d05bf52e3add95d6e770a8290be508949d65cd
zerozawa <zekaixie37@gmail.com>
2025-04-08 21:04:34 +0800

    feat: 是的孩子 你怎么知道我买了 FIIO UTWS5 和 水月雨blessing3

--- system/audio.nix
+++ system/audio.nix
@@ -13,8 +13,31 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
+      jack.enable = true;
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          "wh-1000xm3-ldac-hq" = {
+            "monitor.bluez.rules" = [
+              {
+                matches = [
+                  {
+                    # `pactl list sinks` 查看
+                    "device.name" = "~bluez_card.*";
+                    "media.name" = "FIIO UTWS5";
+                  }
+                ];
+                actions = {
+                  update-props = {
+                    # Set quality to high quality instead of the default of auto
+                    "bluez5.a2dp.ldac.quality" = "hq";
+                  };
+                };
+              }
+            ];
+          };
+        };
+      };
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
       #media-session.enable = true;
