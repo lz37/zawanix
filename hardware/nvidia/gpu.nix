fda5ce334d8f7005070800f474e359a9c0b39c0d
zerozawa <zekaixie37@gmail.com>
2025-04-16 01:19:04 +0800

    feat: update

--- hardware/nvidia/gpu.nix
+++ hardware/nvidia/gpu.nix
@@ -36,9 +36,9 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
-      reverseSync.enable = isIntelGPU || isAmdGPU;
+      sync.enable = isIntelGPU || isAmdGPU;
     };
   };
 }
--- system/audio.nix
+++ system/audio.nix
@@ -19,50 +19,25 @@
       jack.enable = true;
       wireplumber = {
         enable = true;
-        # https://github.com/TLATER/dotfiles/blob/a31d74856710936b398318062f0af6616d994eba/nixos-config/default.nix#L189
         extraConfig = {
-          "50-bluez" = {
+          "wh-1000xm3-ldac-hq" = {
             "monitor.bluez.rules" = [
               {
-                matches = [ { "device.name" = "~bluez_card.*"; } ];
+                matches = [
+                  {
+                    # `pactl list sinks` 查看
+                    "device.name" = "~bluez_card.*";
+                    "media.name" = "FIIO UTWS5";
+                  }
+                ];
                 actions = {
                   update-props = {
+                    # Set quality to high quality instead of the default of auto
                     "bluez5.a2dp.ldac.quality" = "hq";
-                    "bluez5.auto-connect" = [
-                      "a2dp_sink"
-                      "a2dp_source"
-                    ];
-                    "bluez5.hw-volume" = [
-                      "a2dp_sink"
-                      "a2dp_source"
-                    ];
                   };
                 };
               }
             ];
-            "monitor.bluez.properties" = {
-              "bluez5.roles" = [
-                "a2dp_sink"
-                "a2dp_source"
-                "bap_sink"
-                "bap_source"
-              ];
-              "bluez5.codecs" = [
-                "ldac"
-                "aptx"
-                "aptx_ll_duplex"
-                "aptx_ll"
-                "aptx_hd"
-                "opus_05_pro"
-                "opus_05_71"
-                "opus_05_51"
-                "opus_05"
-                "opus_05_duplex"
-                "aac"
-                "sbc_xq"
-              ];
-              "bluez5.hfphsp-backend" = "none";
-            };
           };
         };
       };
