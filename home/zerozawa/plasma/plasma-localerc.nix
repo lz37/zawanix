e0508d4bb4c015b1cc61f3453e9caf076b90be2e
zerozawa <zekaixie37@gmail.com>
2025-04-15 18:06:13 +0800

    feat: update

--- home/zerozawa/plasma/plasma-localerc.nix
+++ home/zerozawa/plasma/plasma-localerc.nix
@@ -2,7 +2,7 @@
 
 {
   programs.plasma.configFile.plasma-localerc = {
-    Formats."LANG" = "zh_CN.UTF-8";
-    Translations."LANGUAGE" = "zh_CN:zh:en:en_US";
+    Formats.LANG = "zh_CN.UTF-8";
+    Translations.LANGUAGE = "zh_CN:zh:en:en_US";
   };
 }
--- home/zerozawa/plasma/yakuake.nix
+++ home/zerozawa/plasma/yakuake.nix
@@ -1,7 +1,15 @@
 { ... }:
 
 {
-  programs.plasma.shortcuts.yakuake = {
-    toggle-window-state = "Meta+`,F12,展开/折叠 Yakuake 窗口";
+  programs.plasma = {
+    configFile = {
+      yakuakerc = {
+        Dialogs.FirstRun = false;
+        Window.Width = 100;
+      };
+    };
+    shortcuts.yakuake = {
+      toggle-window-state = "Meta+`,F12,展开/折叠 Yakuake 窗口";
+    };
   };
 }
