3e5a615e56350ec4bba17fc172942bf011e363c9
zerozawa <zekaixie37@gmail.com>
2025-04-21 18:48:12 +0800

    feat: update

--- hardware/nvidia/gpu.nix
+++ hardware/nvidia/gpu.nix
@@ -36,7 +36,7 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
     prime = {
       sync.enable = isIntelGPU || isAmdGPU;
     };
