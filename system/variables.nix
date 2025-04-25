32692afee584ae791ade0e3dcbfaec22a2557e9b
zerozawa <zekaixie37@gmail.com>
2025-04-25 10:07:45 +0800

    feat: update

--- system/variables.nix
+++ system/variables.nix
@@ -5,7 +5,7 @@
     localBinInPath = true;
     homeBinInPath = true;
     sessionVariables = {
-      # NIXOS_OZONE_WL = "1"; # 加了这个 kde global menu 就没法用，不加这个xwayland又不跟手 淦
+      NIXOS_OZONE_WL = "1"; # 加了这个 kde global menu 就没法用，不加这个xwayland又不跟手 淦
       LIBVIRT_DEFAULT_URI = "qemu:///system"; # https://github.com/winapps-org/winapps/blob/main/docs/libvirt.md
     };
   };
