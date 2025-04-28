d7b457d2989e2daf712a2c833db9c98a35e54757
zerozawa <zekaixie37@gmail.com>
2025-04-28 10:12:16 +0800

    feat: nh flake

--- system/variables.nix
+++ system/variables.nix
@@ -4,9 +4,11 @@
   environment = {
     localBinInPath = true;
     homeBinInPath = true;
-    sessionVariables = {
+    sessionVariables = rec {
       NIXOS_OZONE_WL = "1"; # 加了这个 kde global menu 就没法用，不加这个xwayland又不跟手 淦
       LIBVIRT_DEFAULT_URI = "qemu:///system"; # https://github.com/winapps-org/winapps/blob/main/docs/libvirt.md
+      NH_OS_FLAKE = (import ../options/variable-pub.nix).path.cfgRoot;
+      NH_FLAKE = NH_OS_FLAKE;
     };
   };
 }
