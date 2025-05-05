d82b4bcd5ad8d24a2e977e5e317597fd26a26e6f
zerozawa <zekaixie37@gmail.com>
2025-05-05 21:56:52 +0800

    feat: del xd

--- home/zerozawa/vscode/common/topics/dotnet.nix
+++ home/zerozawa/vscode/common/topics/dotnet.nix
@@ -4,5 +4,7 @@
   extensions = pkgs.nix4vscode.forVscode [
     "ms-dotnettools.csdevkit"
     "ms-dotnettools.vscodeintellicode-csharp"
+    "ms-dotnettools.csharp"
+    "ms-dotnettools.vscode-dotnet-runtime"
   ];
 }
