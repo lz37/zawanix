bfb9a7080bf9927dfd89f86d8f939f9c0e083f15
zerozawa <zekaixie37@gmail.com>
2025-05-13 00:46:10 +0800

    feat: picacg

--- system/packages/build/picacg.nix
+++ system/packages/build/picacg.nix
@@ -8,8 +8,17 @@ let
   pname = "picacg-qt";
   version = "v1.5.2";
   wrapper = ''
-    #!$out/bin/bash
-    appimage-run $out/share/picacg-qt/picacg-qt.AppImage
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.appimage-run}/bin/appimage-run $out/share/${pname}/${pname}.AppImage
+  '';
+  desktop = ''
+    [Desktop Entry]
+    Type=Application
+    Name=PicACG
+    Exec=${pkgs.appimage-run}/bin/appimage-run $out/share/${pname}/${pname}.AppImage
+    Comment=PicACG
+    Icon=$out/share/icons/hicolor/192x192/pixmaps/PicACG.png
+    Categories=Graphics;
   '';
 in
 {
@@ -30,22 +39,20 @@ in
       };
       buildInputs = [
         pkgs.xorg.libxcb
-        pkgs.appimage-run
-        pkgs.bash
       ];
       installPhase = ''
-        mkdir -p $out/share/picacg-qt
-        cp $src $out/share/picacg-qt/picacg-qt.AppImage
+        export TEMP_INSTALL=$(mktemp -d)
+        mkdir -p $out/share/${pname}
+        cp $src $out/share/${pname}/${pname}.AppImage
         mkdir -p $out/bin
-        echo "${wrapper}" > $out/bin/picacg-qt
-        chmod +x $out/bin/picacg-qt
+        echo "${wrapper}" > $out/bin/${pname}
+        chmod +x $out/bin/${pname}
         mkdir -p $out/share/applications
-        mkdir -p $out/share/icons/hicolor/192x192
-        mkdir -p $out/tmp
-        appimage-run -x $out/tmp $src
-        cp $out/tmp/PicACG.png $out/share/icons/hicolor/192x192/pixmaps/PicACG.png
-        cp $out/tmp/PicACG.desktop $out/share/applications/PicACG.desktop
-        rm -rf $out/tmp
+        mkdir -p $out/share/icons/hicolor/192x192/pixmaps
+        ${pkgs.appimage-run}/bin/appimage-run -x $TEMP_INSTALL $out/share/${pname}/${pname}.AppImage
+        cp $TEMP_INSTALL/PicACG.png $out/share/icons/hicolor/192x192/pixmaps/PicACG.png
+        echo "${desktop}" > $out/share/applications/PicACG.desktop
+        rm -rf $TEMP_INSTALL
       '';
     }
   );
