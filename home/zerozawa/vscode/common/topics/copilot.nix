b50ea5afa5ea246347e37bad628bc891e7a3b3dd
zerozawa <zekaixie37@gmail.com>
2025-05-27 20:25:33 +0800

    feat: vscode copilot & md

--- home/zerozawa/vscode/common/topics/copilot.nix
+++ home/zerozawa/vscode/common/topics/copilot.nix
@@ -6,8 +6,41 @@
     github.copilot-chat
   ];
   settings = {
-    "github.copilot.editor.enableAutoCompletions" = true;
     "chat.agent.enabled" = true;
+    "github.copilot.enable" = {
+      "*" = true;
+      "plaintext" = true;
+      "markdown" = true;
+      "scminput" = false;
+    };
+    "github.copilot.advanced" = {
+      "authPermissions" = "default";
+      "authProvider" = "github";
+      "useLanguageServer" = true;
+    };
+    "github.copilot.editor.enableCodeActions" = true;
+    "github.copilot.nextEditSuggestions.enabled" = true;
+    "github.copilot.nextEditSuggestions.fixes" = true;
+    "github.copilot.renameSuggestions.triggerAutomatically" = true;
+    "github.copilot.chat.agent.thinkingTool" = true;
+    "github.copilot.chat.agent.runTasks" = true;
+    "github.copilot.chat.agent.autoFix" = true;
+    "github.copilot.chat.codesearch.enabled" = true;
+    "github.copilot.chat.completionContext.typescript.mode" = "on";
+    "github.copilot.chat.editor.temporalContext.enabled" = true;
+    "github.copilot.chat.edits.newNotebook.enabled" = true;
+    "github.copilot.chat.edits.temporalContext.enabled" = true;
+    "github.copilot.chat.edits.suggestRelatedFilesFromGitHistory" = true;
+    "github.copilot.chat.edits.suggestRelatedFilesForTests" = true;
+    "github.copilot.chat.followUps" = "always";
+    "github.copilot.chat.languageContext.fix.typescript.enabled" = true;
+    "github.copilot.chat.languageContext.inline.typescript.enabled" = true;
+    "github.copilot.chat.languageContext.typescript.enabled" = true;
+    "github.copilot.chat.search.keywordSuggestions" = true;
+    "github.copilot.chat.terminalChatLocation" = "terminal";
+    "github.copilot.chat.summarizeAgentConversationHistory.enabled" = true;
+    "github.copilot.chat.scopeSelection" = true;
+    "github.copilot.chat.generateTests.codeLens" = true;
   };
   keybindings = [
     {
--- home/zerozawa/vscode/common/topics/markdown.nix
+++ home/zerozawa/vscode/common/topics/markdown.nix
@@ -3,13 +3,16 @@
 {
   extensions = pkgs.nix4vscode.forVscode [
     "yzhang.markdown-all-in-one"
-    # shd101wyy.markdown-preview-enhanced
+    # "shd101wyy.markdown-preview-enhanced"
     "davidanson.vscode-markdownlint"
+    "bierner.markdown-mermaid"
+    "yzane.markdown-pdf"
   ];
   settings = {
     # markdown-preview-enhanced.previewTheme = "atom-dark.css";
     "[markdown]" = {
       "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
     };
+    "markdown-pdf.executablePath" = "${pkgs.chromium}/bin/chromium";
   };
 }
