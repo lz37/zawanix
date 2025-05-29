4fb8b214e1aca338dc2d77e1f7fd293b1b973337
zerozawa <zekaixie37@gmail.com>
2025-05-29 23:30:07 +0800

    fix: enable uBlock Origin Lite

--- home/zerozawa/browser.nix
+++ home/zerozawa/browser.nix
@@ -33,7 +33,7 @@ let
       { id = "nhdogjmejiglipccpnnnanhbledajbpd"; } # Vue.js devtools
       { id = "bgejgfcdaicmfbfphchgcdgnpnbcondb"; } # 扩展管理器
       { id = "gcalenpjmijncebpfijmoaglllgpjagf"; } # 篡改猴测试版
-      # { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite (cr你m什么时候s啊)
+      { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite (cr你m什么时候s啊)
     ];
   };
 in
