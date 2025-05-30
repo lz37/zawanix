6b681be47fa2d59e112b5bf851e7ff5c6aa663b5
zerozawa <zekaixie37@gmail.com>
2025-05-30 15:08:01 +0800

    fix: wechat input method

--- system/packages/build/wechat.nix
+++ system/packages/build/wechat.nix
@@ -9,6 +9,10 @@ let
   version = "4.0.1";
   wrapper = ''
     #!${pkgs.bash}/bin/bash
+    XIM=fcitx
+    GTK_IM_MODULE=fcitx
+    QT_IM_MODULE=fcitx
+    XMODIFIERS=@im=fcitx
     ${pkgs.appimage-run}/bin/appimage-run -w $out/opt/${pname}
   '';
   desktop = ''
@@ -16,7 +20,7 @@ let
     Categories=Utility;
     Comment=Wechat Desktop
     Comment[zh_CN]=微信桌面版
-    Exec=${pkgs.appimage-run}/bin/appimage-run -w $out/opt/${pname}
+    Exec=XIM=fcitx GTK_IM_MODULE=fcitx QT_IM_MODULE=fcitx XMODIFIERS=@im=fcitx ${pkgs.appimage-run}/bin/appimage-run -w $out/opt/${pname}
     Icon=$out/opt/${pname}/.DirIcon
     Name=${pname}
     Name[zh_CN]=微信
