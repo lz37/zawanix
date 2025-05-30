ec8910ce8305a361f835ce53838f27c5e283c2b4
zerozawa <zekaixie37@gmail.com>
2025-05-30 15:28:27 +0800

    fix: drawio setting

--- home/zerozawa/vscode/common/topics/document/drawio.nix
+++ home/zerozawa/vscode/common/topics/document/drawio.nix
@@ -4,4 +4,8 @@
   extensions = pkgs.nix4vscode.forVscode [
     "hediet.vscode-drawio"
   ];
+  settings = {
+    # markdown-preview-enhanced.previewTheme = "atom-dark.css";
+    "hediet.vscode-drawio.resizeImages" = true;
+  };
 }
