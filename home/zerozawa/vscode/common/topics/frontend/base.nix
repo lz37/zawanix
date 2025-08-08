1216885d6172b17fd9f667e49108ff885b0be87a
zerozawa <zekaixie37@gmail.com>
2025-08-08 13:05:59 +0800

    fix: typescript

--- home/zerozawa/vscode/common/topics/frontend/base.nix
+++ home/zerozawa/vscode/common/topics/frontend/base.nix
@@ -1,4 +1,14 @@
 { ram, pkgs, ... }:
+let
+  makeTrue =
+    v:
+    v
+    |> builtins.map (x: {
+      name = x;
+      value = true;
+    })
+    |> builtins.listToAttrs;
+in
 {
   extensions = with pkgs.vscode-selected-extensionsCompatible.vscode-marketplace; [
     dbaeumer.vscode-eslint
@@ -13,7 +23,33 @@
     "typescript.updateImportsOnFileMove.enabled" = "always";
     "javascript.updateImportsOnFileMove.enabled" = "always";
     # "typescript.experimental.useTsgo" = true;
-    "typescript.experimental.expandableHover" = true;
-    "typescript.tsserver.experimental.enableProjectDiagnostics" = true;
-  };
+    "typescript.inlayHints.parameterNames.enabled" = "all";
+  }
+  // (makeTrue [
+    "typescript.experimental.expandableHover"
+    "typescript.tsserver.experimental.enableProjectDiagnostics"
+    "typescript.implementationsCodeLens.enabled"
+    "typescript.implementationsCodeLens.showOnInterfaceMethods"
+    "typescript.referencesCodeLens.enabled"
+    "typescript.referencesCodeLens.showOnAllFunctions"
+    "typescript.enablePromptUseWorkspaceTsdk"
+    "typescript.inlayHints.enumMemberValues.enabled"
+    "typescript.inlayHints.parameterNames.suppressWhenArgumentMatchesName"
+    "typescript.inlayHints.parameterTypes.enabled"
+    "typescript.inlayHints.functionLikeReturnTypes.enabled"
+    "typescript.inlayHints.propertyDeclarationTypes.enabled"
+    "typescript.inlayHints.variableTypes.enabled"
+    "typescript.inlayHints.variableTypes.suppressWhenTypeMatchesName"
+    "typescript.suggest.autoImports"
+    "typescript.suggest.classMemberSnippets.enabled"
+    "typescript.suggest.completeFunctionCalls"
+    "typescript.suggest.completeJSDocs"
+    "typescript.suggest.enabled"
+    "typescript.suggest.includeAutomaticOptionalChainCompletions"
+    "typescript.suggest.jsdoc.generateReturns"
+    "typescript.suggest.paths"
+    "typescript.suggest.includeCompletionsForImportStatements"
+    "typescript.suggest.objectLiteralMethodSnippets.enabled"
+    "typescript.suggestionActions.enabled"
+  ]);
 }
