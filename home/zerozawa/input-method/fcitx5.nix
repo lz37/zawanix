48925d41befc24a8d86130cad987b5fcef16e677
zerozawa <zekaixie37@gmail.com>
2025-09-26 15:24:50 +0800

    feat: rime-japanese

--- home/zerozawa/input-method/fcitx5.nix
+++ home/zerozawa/input-method/fcitx5.nix
@@ -3,9 +3,14 @@
   lib,
   ...
 }: {
-  xdg.dataFile."fcitx5/rime/default.custom.yaml".source = (pkgs.formats.yaml {}).generate "default.custom.yaml" {
-    patch = {
-      __include = "rime_ice_suggestion:/";
+  xdg.dataFile = {
+    "fcitx5/rime/default.custom.yaml".source = (pkgs.formats.yaml {}).generate "default.custom.yaml" {
+      patch = {
+        __include = "rime_ice_suggestion:/";
+        __patch = {
+          schema_list = [{schema = "rime_ice";} {schema = "japanese";}];
+        };
+      };
     };
   };
   i18n.inputMethod = {
@@ -18,86 +23,82 @@
             {
               librime = with pkgs;
                 (pkgs.librime.override {
-                  plugins = [librime-lua librime-octagram];
+                  plugins = [librime-lua librime-octagram nur.repos.xyenon.librime-qjs];
                 }).overrideAttrs (old: {
                   buildInputs = (old.buildInputs or []) ++ [luajit];
                 });
-              rimeDataPkgs = [
-                (pkgs.nur.repos.xddxdd.rime-ice.overrideAttrs
-                  (old: let
-                    dicts_to_added = [
-                      [pkgs.nur.repos.xddxdd.rime-zhwiki ["zhwiki"]]
-                      [pkgs.nur.repos.xddxdd.rime-moegirl ["moegirl"]]
-                      [
-                        pkgs.nur.repos.xddxdd.rime-dict
+              rimeDataPkgs =
+                [pkgs.rime-japanese pkgs.nur.repos.Freed-Wu.rime-kaomoji]
+                ++ [
+                  (pkgs.nur.repos.xddxdd.rime-ice.overrideAttrs
+                    (old: let
+                      dicts_to_added = [
+                        [pkgs.nur.repos.xddxdd.rime-zhwiki ["zhwiki"]]
+                        [pkgs.nur.repos.xddxdd.rime-moegirl ["moegirl"]]
                         [
-                          "luna_pinyin.anime"
-                          "luna_pinyin.classical"
-                          "luna_pinyin.diet"
-                          "luna_pinyin.history"
-                          "luna_pinyin.music"
-                          "luna_pinyin.practical"
-                          "luna_pinyin.basis"
-                          "luna_pinyin.cn_en"
-                          "luna_pinyin.game"
-                          "luna_pinyin.idiom"
-                          "luna_pinyin.name"
-                          "luna_pinyin.sougou"
-                          "luna_pinyin.biaoqing"
-                          "luna_pinyin.computer"
-                          "luna_pinyin.gd"
-                          "luna_pinyin.moba"
-                          "luna_pinyin.net"
-                          "luna_pinyin.website"
-                          "luna_pinyin.chat"
-                          "luna_pinyin.daily"
-                          "luna_pinyin.hanyu"
-                          "luna_pinyin.movie"
-                          "luna_pinyin.poetry"
+                          pkgs.nur.repos.xddxdd.rime-dict
+                          [
+                            "luna_pinyin.anime"
+                            "luna_pinyin.classical"
+                            "luna_pinyin.diet"
+                            "luna_pinyin.history"
+                            "luna_pinyin.music"
+                            "luna_pinyin.practical"
+                            "luna_pinyin.basis"
+                            "luna_pinyin.cn_en"
+                            "luna_pinyin.game"
+                            "luna_pinyin.idiom"
+                            "luna_pinyin.name"
+                            "luna_pinyin.sougou"
+                            "luna_pinyin.biaoqing"
+                            "luna_pinyin.computer"
+                            "luna_pinyin.gd"
+                            "luna_pinyin.moba"
+                            "luna_pinyin.net"
+                            "luna_pinyin.website"
+                            "luna_pinyin.chat"
+                            "luna_pinyin.daily"
+                            "luna_pinyin.hanyu"
+                            "luna_pinyin.movie"
+                            "luna_pinyin.poetry"
+                          ]
                         ]
-                      ]
-                      [pkgs.rime-data ["luna_pinyin" "pinyin_simp"]]
-                      [
-                        (pkgs.nur.repos.xddxdd.rime-custom-pinyin-dictionary.overrideAttrs (old: {
-                          postInstall = ''
-                            echo -e "---\nname: CustomPinyinDictionary\nversion: \"0.0\"\nsort: by_weight\nuse_preset_vocabulary: true\n..." | cat - $out/share/rime-data/CustomPinyinDictionary.dict.yaml > temp && mv temp $out/share/rime-data/CustomPinyinDictionary.dict.yaml
-                          '';
-                        }))
-                        ["CustomPinyinDictionary"]
-                      ]
-                    ];
-                  in {
-                    buildInputs =
-                      (old.buildInputs or [])
-                      ++ (map (d: lib.elemAt d 0) dicts_to_added);
-                    postInstall = lib.concatMapStrings (x: x + "\n") [
-                      (lib.concatMapStrings (d: let
-                        dict_pkg = lib.elemAt d 0;
-                        dict_pkg_name = lib.getName dict_pkg;
-                        dict_files = lib.elemAt d 1;
-                      in
-                        lib.concatMapStrings (dict_file: ''
-                          if [ ! -d $out/share/rime-data/${dict_pkg_name} ]; then
-                            mkdir -p $out/share/rime-data/${dict_pkg_name};
-                          fi
-                          ln -s ${dict_pkg}/share/rime-data/${dict_file}.dict.yaml $out/share/rime-data/${dict_pkg_name}/${dict_file}.dict.yaml
-                          sed -i '/^\.\.\./i\  - ${dict_pkg_name}/${dict_file}' $out/share/rime-data/rime_ice.dict.yaml
-                        '')
-                        dict_files)
-                      dicts_to_added)
-                      # $out/share/rime-data/rime_ice.dict.yaml 中的 `# - cn_dicts/41448` 改为 `- cn_dicts/41448`
-                      ''
-                        sed -i 's/^  # - cn_dicts\/41448\(.*\)$/  - cn_dicts\/41448\1/' $out/share/rime-data/rime_ice.dict.yaml
-                      ''
-                    ];
-                  }))
-              ];
+                        [pkgs.rime-data ["luna_pinyin" "pinyin_simp"]]
+                        [
+                          pkgs.nur.repos.xddxdd.rime-custom-pinyin-dictionary
+                          ["CustomPinyinDictionary"]
+                        ]
+                      ];
+                    in {
+                      buildInputs =
+                        (old.buildInputs or [])
+                        ++ (map (d: lib.elemAt d 0) dicts_to_added);
+                      postInstall = lib.concatMapStrings (x: x + "\n") [
+                        (lib.concatMapStrings (d: let
+                          dict_pkg = lib.elemAt d 0;
+                          dict_pkg_name = lib.getName dict_pkg;
+                          dict_files = lib.elemAt d 1;
+                        in
+                          lib.concatMapStrings (dict_file: ''
+                            if [ ! -d $out/share/rime-data/${dict_pkg_name} ]; then
+                              mkdir -p $out/share/rime-data/${dict_pkg_name};
+                            fi
+                            ln -s ${dict_pkg}/share/rime-data/${dict_file}.dict.yaml $out/share/rime-data/${dict_pkg_name}/${dict_file}.dict.yaml
+                            sed -i '/^\.\.\./i\  - ${dict_pkg_name}/${dict_file}' $out/share/rime-data/rime_ice.dict.yaml
+                          '')
+                          dict_files)
+                        dicts_to_added)
+                        # $out/share/rime-data/rime_ice.dict.yaml 中的 `# - cn_dicts/41448` 改为 `- cn_dicts/41448`
+                        ''
+                          sed -i 's/^  # - cn_dicts\/41448\(.*\)$/  - cn_dicts\/41448\1/' $out/share/rime-data/rime_ice.dict.yaml
+                        ''
+                      ];
+                    }))
+                ];
             })
         ]
         ++ (with pkgs; [
           fcitx5-rime
-          fcitx5-chinese-addons
-          fcitx5-mozc
           fcitx5-gtk
           fcitx5-lua
         ]);
@@ -123,7 +124,6 @@
           };
           "Groups/0/Items/0".Name = "rime";
           "Groups/0/Items/1".Name = "keyboard-us";
-          "Groups/0/Items/2".Name = "mozc";
         };
         globalOptions = {
           Hotkey = {
