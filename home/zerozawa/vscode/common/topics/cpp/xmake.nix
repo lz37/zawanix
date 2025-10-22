4754de541fa2940310a628f3b01d510a53009bde
zerozawa <zekaixie37@gmail.com>
2025-10-22 17:28:09 +0800

    feat: qt

--- home/zerozawa/vscode/common/topics/cpp/xmake.nix
+++ home/zerozawa/vscode/common/topics/cpp/xmake.nix
@@ -1,10 +1,15 @@
 {pkgs, ...}: {
+  # pkg=pkgs.vscode-selected-extensionsCompatible.open-vsx-release.vadimcn.vscode-lldb;
   extensions = with pkgs.vscode-selected-extensionsCompatible.vscode-marketplace; [
     tboox.xmake-vscode
     llvm-vs-code-extensions.vscode-clangd
-    # vadimcn.vscode-lldb
     xaver.clang-format
     ms-vscode.cpptools
+    theqtcompany.qt-core
+    theqtcompany.qt-qml
+    theqtcompany.qt-ui
+    # llvm-vs-code-extensions.lldb-dap
+    # vadimcn.vscode-lldb
   ];
   settings = {
     "[cpp]" = {
