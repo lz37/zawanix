d0d359b52125365ca73106870e9f67134d1ffd0d
zerozawa <zekaixie37@gmail.com>
2025-11-24 16:47:46 +0800

    fix: window rule to_legacyRule

--- home/zerozawa/hyprland/hyprland/windowrules.nix
+++ home/zerozawa/hyprland/hyprland/windowrules.nix
@@ -1,11 +1,58 @@
-{...}: {
+{...}: let
+  to_legacyRule = rule:
+    if builtins.typeOf rule == "string"
+    then rule
+    else let
+      serializeMatch = match: let
+        serializePair = key: value:
+          if builtins.typeOf value == "bool"
+          then "match:${key} ${
+            if value
+            then "1"
+            else "0"
+          }"
+          else "match:${key} ${value}";
+      in
+        builtins.concatStringsSep ", " (
+          builtins.filter (x: x != "") (
+            builtins.map (k: serializePair k (match.${k})) (builtins.attrNames match)
+          )
+        );
+      serializeAction = action: let
+        serializePair = key: value: "${key} ${
+          if builtins.typeOf value == "bool"
+          then
+            (
+              if value
+              then "1"
+              else "0"
+            )
+          else value
+        }";
+      in
+        builtins.concatStringsSep ", " (
+          builtins.map (k: serializePair k (action.${k})) (builtins.attrNames action)
+        );
+    in "${serializeMatch rule.match}, ${serializeAction (builtins.removeAttrs rule ["match"])}";
+  to_legacyRules = rules:
+    builtins.map to_legacyRule rules;
+in {
   wayland.windowManager.hyprland = {
     settings = {
-      windowrule = [
-        "no_blur 1, match:xwayland 1" # Helps prevent odd borders/shadows for xwayland apps
+      windowrule = to_legacyRules [
+        {
+          match = {xwayland = true;};
+          no_blur = true;
+        } # Helps prevent odd borders/shadows for xwayland apps
         # downside it can impact other xwayland apps
         # This rule is a template for a more targeted approach
-        "no_blur 1, match:class ^(resolve)$, match:xwayland 1" # Window rule for just resolve
+        {
+          match = {
+            xwayland = true;
+            class = "^(\\bresolve\\b)$";
+          };
+          no_blur = true;
+        } # Window rule for just resolve
         "tag +file-manager, match:class ^([Tt]hunar|org.gnome.Nautilus|[Pp]cmanfm-qt)$"
         "tag +terminal, match:class ^(com.mitchellh.ghostty|org.wezfurlong.wezterm|Alacritty|kitty|kitty-dropterm)$"
         "tag +browser, match:class ^(Brave-browser(-beta|-dev|-unstable)?)$"
@@ -33,51 +80,233 @@
         "tag +settings, match:class (xdg-desktop-portal-gtk)"
         "tag +settings, match:class (.blueman-manager-wrapped)"
         "tag +settings, match:class (nwg-displays)"
-        "move 72% 7%, match:title ^(Picture-in-Picture)$"
-        "center 1, match:class ^([Ff]erdium)$"
-        "float 1, match:class ^([Ww]aypaper)$"
-        "center 1, match:class ^(pavucontrol|org.pulseaudio.pavucontrol|com.saivert.pwvucontrol)$"
-        "center 1, match:class ([Tt]hunar), match:title negative:(.*[Tt]hunar.*)"
-        "center 1, match:title ^(Authentication Required)$"
-        "idle_inhibit fullscreen, match:class ^(*)$"
-        "idle_inhibit fullscreen, match:title ^(*)$"
-        "idle_inhibit fullscreen, match:fullscreen 1"
-        "float 1, match:tag settings*"
-        "float 1, match:class ^([Ff]erdium)$"
-        "float 1, match:title ^(Picture-in-Picture)$"
-        "float 1, match:class ^(mpv|com.github.rafostar.Clapper)$"
-        "float 1, match:title ^(Authentication Required)$"
-        "float 1, match:class (codium|codium-url-handler|VSCodium), match:title negative:(.*codium.*|.*VSCodium.*)"
-        "float 1, match:class ^(com.heroicgameslauncher.hgl)$, match:title negative:(Heroic Games Launcher)"
-        "float 1, match:class ^([Ss]team)$, match:title negative:^([Ss]team)$"
-        "float 1, match:class ([Tt]hunar), match:title negative:(.*[Tt]hunar.*)"
-        "float 1, match:initial_title (Add Folder to Workspace)"
-        "float 1, match:initial_title (Open Files)"
-        "float 1, match:initial_title (wants to save)"
-        "size 70% 60%, match:initial_title (Open Files)"
-        "size 70% 60%, match:initial_title (Add Folder to Workspace)"
-        "size 70% 70%, match:tag settings*"
-        "size 60% 70%, match:class ^([Ff]erdium)$"
-        "opacity 1.0 1.0, match:tag browser*"
-        "opacity 0.9 0.8, match:tag projects*"
-        "opacity 0.94 0.86, match:tag im*"
-        "opacity 0.9 0.8, match:tag file-manager*"
-        "opacity 0.8 0.7, match:tag terminal*"
-        "opacity 0.8 0.7, match:tag settings*"
-        "opacity 0.8 0.7, match:class ^(gedit|org.gnome.TextEditor|mousepad)$"
-        "opacity 0.9 0.8, match:class ^(seahorse)$ # gnome-keyring gui"
-        "opacity 0.95 0.75, match:title ^(Picture-in-Picture)$"
-        "pin 1, match:title ^(Picture-in-Picture)$"
-        "keep_aspect_ratio 1, match:title ^(Picture-in-Picture)$"
-        "no_blur 1, match:tag games*"
-        "fullscreen 1, match:tag games*"
-        "fullscreen 1, match:class Waydroid"
-        "float 1, match:title ^([Vv]ivaldi ((Settings)|(设置)).*))$"
-        "float 1, match:class wechat"
-        "float 1, match:class QQ"
-        "float 1, match:title ^(DevTools.*)$, match:initial_class ^((code)|(chromium-browser))$"
-        "opacity 0.0 override, match:class xwaylandvideobridge"
-        "size 0% 0%, match:class xwaylandvideobridge"
+        {
+          match = {
+            title = "^(Picture-in-Picture)$";
+          };
+          move = "72% 7%";
+          float = true;
+          opacity = "0.95 0.75";
+          pin = true;
+          keep_aspect_ratio = true;
+        }
+        {
+          match = {
+            class = "^([Ff]erdium)$";
+          };
+          center = true;
+        }
+        {
+          match = {
+            class = "^([Ww]aypaper)$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "^(pavucontrol|org.pulseaudio.pavucontrol|com.saivert.pwvucontrol)$";
+          };
+          center = true;
+        }
+        {
+          match = {
+            class = "([Tt]hunar)";
+            title = "negative:(.*[Tt]hunar.*)";
+          };
+          center = true;
+        }
+        {
+          match = {
+            title = "^(Authentication Required)$";
+          };
+          center = true;
+        }
+        {
+          match = {
+            class = "^(*)$";
+          };
+          idle_inhibit = "fullscreen";
+        }
+        {
+          match = {
+            title = "^(*)$";
+          };
+          idle_inhibit = "fullscreen";
+        }
+        {
+          match = {
+            fullscreen = true;
+          };
+          idle_inhibit = "fullscreen";
+        }
+        {
+          match = {
+            tag = "settings*";
+          };
+          float = true;
+          size = "70% 70%";
+          opacity = "0.8 0.7";
+        }
+        {
+          match = {
+            class = "^([Ff]erdium)$";
+          };
+          float = true;
+          size = "60% 70%";
+        }
+        {
+          match = {
+            class = "^(mpv|com.github.rafostar.Clapper)$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            title = "^(Authentication Required)$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "(codium|codium-url-handler|VSCodium)";
+            title = "negative:(.*codium.*|.*VSCodium.*)";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "^(com.heroicgameslauncher.hgl)$";
+            title = "negative:(Heroic Games Launcher)";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "^([Ss]team)$";
+            title = "negative:^([Ss]team)$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "([Tt]hunar)";
+            title = "negative:(.*[Tt]hunar.*)";
+          };
+          float = true;
+        }
+        {
+          match = {
+            initial_title = "(Add Folder to Workspace)";
+          };
+          float = true;
+          size = "70% 60%";
+        }
+        {
+          match = {
+            initial_title = "(Open Files)";
+          };
+          float = true;
+          size = "70% 60%";
+        }
+        {
+          match = {
+            initial_title = "(wants to save)";
+          };
+          float = true;
+        }
+        {
+          match = {
+            tag = "browser*";
+          };
+          opacity = "1.0 1.0";
+        }
+        {
+          match = {
+            tag = "projects*";
+          };
+          opacity = "0.9 0.8";
+        }
+        {
+          match = {
+            tag = "im*";
+          };
+          opacity = "0.94 0.86";
+        }
+        {
+          match = {
+            tag = "file-manager*";
+          };
+          opacity = "0.9 0.8";
+        }
+        {
+          match = {
+            tag = "terminal*";
+          };
+          opacity = "0.8 0.7";
+        }
+        {
+          match = {
+            class = "^(gedit|org.gnome.TextEditor|mousepad)$";
+          };
+          opacity = "0.8 0.7";
+        }
+        {
+          match = {
+            class = "^(seahorse)$";
+          };
+          opacity = "0.9 0.8";
+        } # gnome-keyring gui
+        {
+          match = {
+            class = "^(mpv|com.github.rafostar.Clapper)$";
+          };
+          opacity = "1.0 1.0";
+        }
+        {
+          match = {
+            tag = "games*";
+          };
+          no_blur = true;
+          fullscreen = true;
+        }
+        {
+          match = {
+            class = "Waydroid";
+          };
+          fullscreen = true;
+        }
+        {
+          match = {
+            class = "^([Vv]ivaldi ((Settings)|(设置)).*))$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "wechat";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "QQ";
+          };
+          float = true;
+        }
+        {
+          match = {
+            title = "^(DevTools.*)$";
+            initial_class = "^((code)|(chromium-browser))$";
+          };
+          float = true;
+        }
+        {
+          match = {
+            class = "^(xwaylandvideobridge)$";
+          };
+          opacity = "0.0 override";
+          size = "0% 0%";
+        }
       ];
     };
   };
