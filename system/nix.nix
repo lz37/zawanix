16856386f77ab6579a79cec443098cc9e2af8a5e
zerozawa <zekaixie37@gmail.com>
2026-04-14 00:04:42 +0800

    chore: reprioritize nix mirrors
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- system/nix.nix
+++ system/nix.nix
@@ -26,11 +26,11 @@
         # 中科大
         "https://mirrors.ustc.edu.cn/nix-channels/store?priority=10"
         # 上交
-        "https://mirror.sjtu.edu.cn/nix-channels/store?priority=10"
-        # 中科院
-        "https://mirror.iscas.ac.cn/nix-channels/store?priority=10"
+        # "https://mirror.sjtu.edu.cn/nix-channels/store?priority=10"
         # 清华
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=11"
+        # 中科院
+        "https://mirror.iscas.ac.cn/nix-channels/store?priority=11"
         # 南大
         "https://mirror.nju.edu.cn/nix-channels/store?priority=11"
         # 北外
