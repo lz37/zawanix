75da6ce02d59cb059c00f30a52c7bc9230fa2ebe
zerozawa <zekaixie37@gmail.com>
2026-06-14 02:33:22 +0800

    chore(env): Add npm_config_nodedir for node-gyp compatibility
    - Set npm_config_nodedir to master nodejs include directory to fix native module builds.
    
    Co-authored-by: OH-MY-PI <omp.sh>

--- system/variables.nix
+++ system/variables.nix
@@ -1,9 +1,14 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   environment = {
     localBinInPath = true;
     homeBinInPath = true;
     sessionVariables = rec {
-      NIXOS_OZONE_WL = "1"; # 加了这个 kde global menu 就没法用，不加这个xwayland又不跟手 淦
+      NIXOS_OZONE_WL = "1";
+      npm_config_nodedir = "${pkgs.master.nodejs}/include/node";
       LIBVIRT_DEFAULT_URI = "qemu:///system"; # https://github.com/winapps-org/winapps/blob/main/docs/libvirt.md
       NH_OS_FLAKE = config.zerozawa.path.cfgRoot;
       NH_FLAKE = NH_OS_FLAKE;
